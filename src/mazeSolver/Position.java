fd076ec77323cc4a6d92b706a16a6cebc8684a35
Tzoymas Michael <mixalhstzoumas@gmail.com>
2025-04-28 16:21:56 +0300

    Update Position.java

--- src/mazeSolver/Position.java
+++ src/mazeSolver/Position.java
@@ -3,7 +3,7 @@ package mazeSolver;
 public class Position {
 		public int x;
 		public int y;
-		public Position(int x, int y) {			//ΔΟΜΗΤΗΣ
+		public Position(int y, int x) {			//ΔΟΜΗΤΗΣ
 			this.x=x;							//Το this.x αναφέρεται στις μεταβλητές της κλάσης. Το χ στις παραμέτρους
 			this.y=y;							//Το this.y αναφέρεται στις μεταβλητές της κλάσης. Το y στις παραμέτρους					
 		}
